506f0093de95f55e3029400cd73aaeb7f9c6676e
Priyadarshan <priyadarshan.patilmdh@gmail.com>
2021-10-24 11:52:00 +0530

    added code to read the input journey

--- tigerCardAssignment/src/main/java/com/sahaj/assignment/tigerCard/RunTigerCardApp.java
+++ tigerCardAssignment/src/main/java/com/sahaj/assignment/tigerCard/RunTigerCardApp.java
@@ -4,12 +4,22 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.sahaj.assignment.tigerCard.pojos.FromZoneToZone;
 import com.sahaj.assignment.tigerCard.pojos.PeakHourTimings;
 import com.sahaj.assignment.tigerCard.pojos.PeakHourTimingsMasterManager;
+import com.sahaj.assignment.tigerCard.pojos.TravelTime;
 import com.sahaj.assignment.tigerCard.pojos.ZoneTravelMaster;
 import com.sahaj.assignment.tigerCard.pojos.ZoneTravelMasterManager;
+import com.sahaj.assignment.tigerCard.visitables.DayJourneyNode;
+import com.sahaj.assignment.tigerCard.visitables.IJourneyNode;
+import com.sahaj.assignment.tigerCard.visitables.OverallJourneyNode;
+import com.sahaj.assignment.tigerCard.visitables.SingleJourneyNode;
+import com.sahaj.assignment.tigerCard.visitables.WeekJourneyNode;
 
 public class RunTigerCardApp {
 
@@ -28,6 +38,13 @@ public class RunTigerCardApp {
 	public static final int ZONE_MASTER_WEEKLY_CAP_INDEX = 5;
 	
 	
+	//#Inputformat >> day,time,fromZone,toZone
+	public static final int INPUT_JOURNEY_DAY_INDEX = 0;
+	public static final int INPUT_JOURNEY_TIME_INDEX = 1;
+	public static final int INPUT_JOURNEY_FROMZONE_INDEX = 2;
+	public static final int INPUT_JOURNEY_TOZONE_INDEX = 3;
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -38,11 +55,14 @@ public class RunTigerCardApp {
 		System.out.println(inputFileFolder);
 		
 		RunTigerCardApp runTigerCardApp = new RunTigerCardApp();
-		runTigerCardApp.readPeakHourTimings(inputFileFolder, "peakHourMaster.txt");
-		runTigerCardApp.readZoneFareMasterData(inputFileFolder, "zoneFareMaster.txt");
+		//runTigerCardApp.readPeakHourTimings(inputFileFolder, "peakHourMaster.txt");
+		//runTigerCardApp.readZoneFareMasterData(inputFileFolder, "zoneFareMaster.txt");
+		
+		IJourneyNode iJourneyNode = runTigerCardApp.readInputJourney(inputFileFolder, "inputjourneys.txt");
 		
 		Object instance1 = PeakHourTimingsMasterManager.INSTANCE;
 		Object instance2 = ZoneTravelMasterManager.INSTANCE;
+		
 		System.out.println("Print peakHour:" + PeakHourTimingsMasterManager.INSTANCE);
 		System.out.println("Print zoneFareMaster:" + ZoneTravelMasterManager.INSTANCE);
 		
@@ -166,5 +186,100 @@ public class RunTigerCardApp {
 			e.printStackTrace();
 		} 
 	}
+	
+	
+	@SuppressWarnings("unused")
+	public IJourneyNode readInputJourney(String inputFileFolder, String fileName)
+	{
+		
+		File inputJourneyFile = new File(inputFileFolder, fileName);
+		int lineCount = 0;
+		String currentLineRead = null;
+		
+		Map<String, DayJourneyNode> dayJourneyNodeMap = new HashMap<String, DayJourneyNode>();
+		Map<Integer,WeekJourneyNode> weekJourneyNodeMap = new HashMap<Integer, WeekJourneyNode>();
+		int weekCounter = 0;
+		
+		OverallJourneyNode overallJourneyNode = new OverallJourneyNode();
+		
+		try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(inputJourneyFile)) ){
+			
+			
+			while((currentLineRead = bufferedFileReader.readLine()) != null)
+			{
+				if( lineCount == 0)
+				{
+					lineCount++;
+					continue;
+				}
+				
+				String[] inputJourneyData = currentLineRead.split(",");
+				
+				if( inputJourneyData.length < 4)
+				{
+					throw new IllegalArgumentException("Wrong Input format type for Input Journey");
+				}
+				
+				String fromZone= null, toZone= null, journeyDay= null, journeyTime = null;
+				
+				String lastJourneyDay = null;
+				
+				for(int index = 0; index <inputJourneyData.length; index++)
+				{	
+					fromZone = index == INPUT_JOURNEY_FROMZONE_INDEX ? inputJourneyData[INPUT_JOURNEY_FROMZONE_INDEX] : fromZone;
+					toZone = index == INPUT_JOURNEY_TOZONE_INDEX ? inputJourneyData[INPUT_JOURNEY_TOZONE_INDEX] :toZone;
+					journeyDay = index == INPUT_JOURNEY_DAY_INDEX ? inputJourneyData[INPUT_JOURNEY_DAY_INDEX] : journeyDay;
+					journeyTime = index == INPUT_JOURNEY_TIME_INDEX ? inputJourneyData[INPUT_JOURNEY_TIME_INDEX] : journeyTime;
+				
+				}
+				
+				
+				FromZoneToZone fromZoneToZone = new FromZoneToZone(fromZone, toZone);
+				TravelTime travelTime = new TravelTime(journeyDay, journeyTime);
+				SingleJourneyNode singleJourneyNode = new SingleJourneyNode.SingleJourneyNodeBuilder().
+																fromZoneToZone(fromZoneToZone).travelTime(travelTime).build();
+				
+				String dayJourneyNodeMapKey = journeyDay + weekCounter;
+				DayJourneyNode dayJourneyNode =null;
+				if( (dayJourneyNode = dayJourneyNodeMap.get(dayJourneyNodeMapKey)) == null)
+				{
+					dayJourneyNode = new DayJourneyNode(journeyDay);
+					dayJourneyNodeMap.put(dayJourneyNodeMapKey, dayJourneyNode);
+				}
+				
+				dayJourneyNode.addSingleJourneyNode(singleJourneyNode);
+				
+				if(lastJourneyDay != null && !lastJourneyDay.equals("Monday") && journeyDay.equals("Monday") )
+				{
+					weekCounter++;
+				}
+				
+				WeekJourneyNode weekJourneyNode = null;
+				if((weekJourneyNode = weekJourneyNodeMap.get(weekCounter)) == null)
+				{
+					weekJourneyNode= new WeekJourneyNode();
+					weekJourneyNodeMap.put(weekCounter, weekJourneyNode);
+					overallJourneyNode.addWeekJourneyNode(weekJourneyNode);
+					
+				}
+				
+				weekJourneyNode.addDayJourneyNode(dayJourneyNode);
+				
+				lastJourneyDay = journeyDay;
+				
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return overallJourneyNode;
+	}
+	
+	
+	
 
 }
