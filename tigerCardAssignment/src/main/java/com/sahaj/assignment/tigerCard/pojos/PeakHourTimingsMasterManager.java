7f8b897ce4a722e5cf8e466163d54ca0cd11bb71
Priyadarshan <priyadarshan.patilmdh@gmail.com>
2021-10-24 12:58:01 +0530

    Refactored code for file reader

--- tigerCardAssignment/src/main/java/com/sahaj/assignment/tigerCard/pojos/PeakHourTimingsMasterManager.java
+++ tigerCardAssignment/src/main/java/com/sahaj/assignment/tigerCard/pojos/PeakHourTimingsMasterManager.java
@@ -1,12 +1,23 @@
 package com.sahaj.assignment.tigerCard.pojos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public enum PeakHourTimingsMasterManager {
 	
+	
 	INSTANCE;
+	
+	public static final int PEAK_HOUR_TIMING_DAY_CATEGORY_INDEX = 0;
+	public static final int PEAK_HOUR_TIMING_START_TIME_INDEX = 1;
+	public static final int PEAK_HOUR_TIMING_END_TIME_INDEX = 2;
+	
+	
 	
 	List<PeakHourTimings> weekDayPeakHourTimings = new ArrayList<PeakHourTimings>();
 	List<PeakHourTimings> weekendPeakHourTimings = new ArrayList<PeakHourTimings>();
@@ -30,11 +41,80 @@ public enum PeakHourTimingsMasterManager {
 	}
 	
 	
-	
 	public List<PeakHourTimings> getWeekendPeakHourTimings()
 	{
 		return Collections.unmodifiableList(weekendPeakHourTimings);
 	} 
 	
+	
+	public void initializePeakHourTimingMaster(String inputFileFolder, String fileName)
+	{
+		
+		File peakHourTimingFile = new File(inputFileFolder, fileName);
+		int lineCount = 0;
+		String currentLineRead = null;
+		
+		try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(peakHourTimingFile)) ){
+			
+			
+			while((currentLineRead = bufferedFileReader.readLine()) != null)
+			{
+				if( lineCount == 0)
+				{
+					lineCount++;
+					continue;
+				}
+				
+				String[] peakHourData = currentLineRead.split(",");
+				
+				validateInput(peakHourData);
+				
+				createPeakHourTimingsMaster(peakHourData);
+				
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+
+
+	private void validateInput(String[] peakHourData) {
+		if( peakHourData.length < 3)
+		{
+			throw new IllegalArgumentException("Wrong Input format type for Peak Hour Master Data");
+		}
+	}
+	
+	
+	private void createPeakHourTimingsMaster(String[] peakHourData)
+	{
+		boolean isWeekDayCategoryData = false;
+		String peakHourStart = null;
+		String peakHourEnd = null;
+		
+		for(int index = 0; index <peakHourData.length; index++)
+		{	
+			isWeekDayCategoryData = index == PEAK_HOUR_TIMING_DAY_CATEGORY_INDEX && peakHourData[index].equals("weekday") ? true:isWeekDayCategoryData;
+			peakHourStart = index == PEAK_HOUR_TIMING_START_TIME_INDEX ? peakHourData[index] : peakHourStart;
+			peakHourEnd = index == PEAK_HOUR_TIMING_END_TIME_INDEX ? peakHourData[index] : peakHourEnd;
+		
+		}
+		
+		PeakHourTimings peakHourTimings = new PeakHourTimings(peakHourStart, peakHourEnd);
+		
+		if( isWeekDayCategoryData)
+		{
+			PeakHourTimingsMasterManager.INSTANCE.addWeekDayPeakHourTimings(peakHourTimings);
+		}
+		else
+		{
+			PeakHourTimingsMasterManager.INSTANCE.addWeekendPeakHourTimings(peakHourTimings);
+		}
+	}
 
 }
--- tigerCardAssignment/src/main/java/com/sahaj/assignment/tigerCard/pojos/ZoneTravelMasterManager.java
+++ tigerCardAssignment/src/main/java/com/sahaj/assignment/tigerCard/pojos/ZoneTravelMasterManager.java
@@ -1,5 +1,9 @@
 package com.sahaj.assignment.tigerCard.pojos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,6 +12,14 @@ public enum ZoneTravelMasterManager
 {
 	
 	INSTANCE;
+	
+	//fromZone,toZone,peakHourFare,offPeakHourFare,dailyCap,weeklyCap
+	public static final int ZONE_MASTER_FROM_ZONE_INDEX = 0;
+	public static final int ZONE_MASTER_TO_ZONE_INDEX = 1;
+	public static final int ZONE_MASTER_PEAK_HOUR_FARE_INDEX = 2;
+	public static final int ZONE_MASTER_OFF_PEAK_HOUR_INDEX = 3;
+	public static final int ZONE_MASTER_DAILY_CAP_INDEX = 4;
+	public static final int ZONE_MASTER_WEEKLY_CAP_INDEX = 5;
 	
 	private Map<FromZoneToZone, ZoneTravelMaster> zoneTravelMasterData = new HashMap<FromZoneToZone, ZoneTravelMaster>();
 	
@@ -40,5 +52,77 @@ public enum ZoneTravelMasterManager
 		return Collections.unmodifiableMap(zoneTravelMasterData);
 	}
 	
+	
+	public void initializeZoneFareMasterData(String inputFileFolder, String fileName)
+	{
+		
+		File zoneFareMasterDataFile = new File(inputFileFolder, fileName);
+		int lineCount = 0;
+		String currentLineRead = null;
+		
+		try (BufferedReader bufferedFileReader = new BufferedReader(new FileReader(zoneFareMasterDataFile)) ){
+			
+			
+			while((currentLineRead = bufferedFileReader.readLine()) != null)
+			{
+				if( lineCount == 0)
+				{
+					lineCount++;
+					continue;
+				}
+				
+				String[] zoneFareMasterData = currentLineRead.split(",");
+				
+				validateInput(zoneFareMasterData);
+				
+				
+				createZoneTravelMaster(zoneFareMasterData);
+				
+				
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+
+	private void validateInput(String[] zoneFareMasterData) {
+		if( zoneFareMasterData.length < 5)
+		{
+			throw new IllegalArgumentException("Wrong Input format type for Zone Fare Master Data");
+		}
+	}
+	
+	private void createZoneTravelMaster(String[] zoneFareMasterData)
+	{
+		String fromZone = null, toZone= null;
+		double peakHourFare = 0, offPeakHourFare = 0, dailyCapFare = 0, weeklyCapFare = 0;
+		
+		
+		for(int index = 0; index <zoneFareMasterData.length; index++)
+		{	
+			fromZone = index == ZONE_MASTER_FROM_ZONE_INDEX ? zoneFareMasterData[ZONE_MASTER_FROM_ZONE_INDEX] : fromZone;
+			toZone = index == ZONE_MASTER_TO_ZONE_INDEX ? zoneFareMasterData[ZONE_MASTER_TO_ZONE_INDEX] :toZone;
+			peakHourFare = index == ZONE_MASTER_PEAK_HOUR_FARE_INDEX ? Double.parseDouble(zoneFareMasterData[ZONE_MASTER_PEAK_HOUR_FARE_INDEX]) : peakHourFare;
+			offPeakHourFare = index == ZONE_MASTER_OFF_PEAK_HOUR_INDEX ? Double.parseDouble(zoneFareMasterData[ZONE_MASTER_OFF_PEAK_HOUR_INDEX]) : offPeakHourFare;
+			dailyCapFare = index == ZONE_MASTER_DAILY_CAP_INDEX ? Double.parseDouble(zoneFareMasterData[ZONE_MASTER_DAILY_CAP_INDEX]) : dailyCapFare;
+			weeklyCapFare = index == ZONE_MASTER_WEEKLY_CAP_INDEX ? Double.parseDouble(zoneFareMasterData[ZONE_MASTER_WEEKLY_CAP_INDEX]) : weeklyCapFare;
+		
+		}
+		
+		FromZoneToZone fromZoneToZone = new FromZoneToZone(fromZone, toZone);
+		ZoneTravelMaster zoneTravelMaster = new ZoneTravelMaster.ZoneTravelMasterBuilder().
+													dailyCapFare(dailyCapFare).
+														fromZone(fromZoneToZone).
+															offPeakHourFare(offPeakHourFare).
+																peakHourFare(peakHourFare).weeklyCapFare(weeklyCapFare).build();
+		
+		ZoneTravelMasterManager.INSTANCE.addZoneTravelMaster(fromZoneToZone, zoneTravelMaster);
+		
+	}
 
 }
